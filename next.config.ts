1eddb12a03aac6f94a3c82c62e665f853955c29c
Jazzy Gasper <jasmina.gasper@gmail.com>
2025-10-06 11:55:15 -0700

    remove src

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "",
-  output: "export",  // <=== enables static exports
+  output: "export",  // enables static exports
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
